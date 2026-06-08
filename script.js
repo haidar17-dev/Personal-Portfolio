40c47ee3a49e4e9db76335eca59886e11750c8e5
haidar17-dev <adeelhaidar95@gmail.com>
2026-06-08 18:41:20 +0700

    .

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ const menu = document.getElementById('mobileMenu');
 btn.addEventListener('click', () => {
   menu.classList.toggle('open');
 });
+
+
+function changeImg(thumb) {
+  const main = document.getElementById('main-img');
+  main.style.opacity = 0;
+  main.style.transform = 'scale(0.95)';
+  setTimeout(() => {
+    main.src = thumb.src;
+    main.style.opacity = 1;
+    main.style.transform = 'scale(1)';
+  }, 300);
+}
